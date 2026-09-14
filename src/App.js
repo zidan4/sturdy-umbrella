7285433d7ff7f8334b76b0c1726bf083062edf32
zidan4 <zidanjaffar@gmail.com>
2026-09-14 21:00:18 +0300

    children

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import  { Greet }  from './components/greet';
 import GreetClass from './components/Greet';
 import Pro from './components/Pro.mjs';
 import Cl from './components/cl'
+import  Child  from './components/children'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           <Greet />
           <GreetClass />
           <Pro name="Pablovic" />
           <Cl name='eryc' />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Child>
+          <ul>
+            <li>first child</li>
+            <li>second child</li>
+            <li>last child</li>
+          </ul>
+        </Child>
       </header>
     </div>
   );
---
+++ src/components/children.js
@@ -0,0 +1,5 @@
+const Child = ( props ) => {
+  return (<p> {props.children} </p>)
+}
+
+export default Child
